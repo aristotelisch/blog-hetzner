de7ef7f0c99baab5d40b8c7488e7cae09049d33e
Aristotelis Christou <telischristou@gmail.com>
2013-10-10 17:12:55 +0300

    add header and flash message sections to application layout, add routes for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131010124457) do
+ActiveRecord::Schema.define(version: 20131010125026) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "firstname"
+    t.string   "lastname"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
