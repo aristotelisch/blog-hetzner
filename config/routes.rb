8bcb827331078acc9327eb1648bec9b534cfc913
Aristotelis Christou <telischristou@gmail.com>
2013-10-12 23:41:06 +0300

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
   root to: "articles#index"
   resources :articles
+  get "/login" => "sessions#new", as: :login 
+  delete "/logout" => "sessions#destroy", as: :logout
   resources :users, only: [:index, :new, :create]
   resource :session, only: [:new, :create, :destroy]
 end
