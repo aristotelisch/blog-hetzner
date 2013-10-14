1c4d829e14e62afc6099553d20efcb7668680650
Aristotelis Christou <telischristou@gmail.com>
2013-10-14 20:09:41 +0300

    fix user_id nil bug when adding articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,8 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
   def new
-    @article = Article.new
+    user = current_user
+    @article = user.articles.new
   end
 
   # GET /articles/1/edit
@@ -71,6 +72,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :user_id)
     end
 end
