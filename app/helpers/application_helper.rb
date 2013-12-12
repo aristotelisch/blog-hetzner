033fd4a80b02fb61213c8d5470802b996d590d3d
Aristotelis Christou <telischristou@gmail.com>
2013-12-12 22:08:08 +0200

    Add dynamic title to every page, add helper make_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+
+  # Δημιούργησε τον τιτλο.
+  # Αν δεν υπάρχει δυναμικός τιτλος εμφάνισε μόνο τον τιτλο του blog
+  # Αν υπάρχει δυναμικός τίτλος εμφάνισε τον τιτλο του blog και τον δυναμικό
+  def make_title(page_title)
+    default_title = "Happybit.eu Blog"
+
+    if page_title.empty?
+      default_title
+    else
+      default_title + " | " + page_title
+    end
+  end
 end
