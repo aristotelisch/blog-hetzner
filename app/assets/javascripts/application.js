0b185ecdf324026025eaca3ada710b0cfb85dc6c
Aristotelis Christou <telischristou@gmail.com>
2014-01-15 20:58:56 +0200

    Alerts fade to 0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,4 @@
 
 
 $(".alert").alert();
-window.setTimeout(function() { $(".alert").fadeTo(500,100).slideUp(500, function() { $(this).remove() })}, 2000);
+window.setTimeout(function() { $(".alert").fadeTo(500,0).slideUp(500, function() { $(this).remove() })}, 2000);
