41263688473613265a679c1dccdd6fbd303e7e1d
Aristotelis Christou <telischristou@gmail.com>
2014-04-11 05:05:44 +0300

    Add default session secret if relevant environment variable is nil

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blog::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
+Blog::Application.config.secret_key_base = ENV["SECRET_TOKEN"] || "ab0d4a4ce6a0e08fa505144571d44adfd257a1feaba893d302066a2ec4d442c3da61f0072fb30d3aa61479da5fa41b17d3c6a4703d4fff70e78dc95536919b74"
