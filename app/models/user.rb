89309faf88e7868da0349fca2630f492f396482f
Aristotelis Christou <telischristou@gmail.com>
2015-03-14 22:57:42 +0200

    validate uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 #  attr_accessible :firstname, :lastname, :email, :username
   has_secure_password
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username
   
   has_many :articles
 end
