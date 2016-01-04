d7192118231026bc1df410aa9a6a95f868cfcd4a
Aristotelis Christou <telischristou@gmail.com>
2016-01-04 12:53:56 +0200

    Move errbit credentials to env variables

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.host = 'http://errors.happybit.eu'
+  config.host = ENV['AIRBRAKE_URL']
   config.project_id = true
-  config.project_key = 'f0529065c615101824841d70617686cd'
+  config.project_key = ENV['AIRBRAKE_KEY']
 end
